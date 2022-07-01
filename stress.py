209223a669180959d550dd997d1b893c5c1523fa
RxnAch <achrxn@gmail.com>
2022-07-01 18:21:31 +0545

    first commit

---
+++ stress.py
@@ -0,0 +1,45 @@
+from flask import Flask,render_template,url_for
+from forms import RegistrationForm,LoginForm
+app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
+@app.route('/')
+@app.route('/home')
+@app.route('/teams')
+def teams():
+    return render_template('teams.html',title="Teams")
+
+# @app.route('/intro')
+# @app.route('/introduction')
+# def intro():
+#     return render_template('introduction.html',title="Introduction")
+
+
+# @app.route('/introdetails')
+# def introdetails():
+#     return render_template('introdetails.html',title="IntroDetails")
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
