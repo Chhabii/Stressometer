1b918652f96a9b2051f3407d3c411aab2bff4669
BharatThapa99 <57470581+BharatThapa99@users.noreply.github.com>
2022-07-15 09:40:56 +0545

    Login completed + others
    others:
    check if the entered username and email are unique in the registration form.
    redirect the user to dashboard if logged in..(dashboard page under construction.

--- stress.py
+++ stress.py
@@ -1,9 +1,11 @@
+from flask import Flask,render_template,url_for,flash,redirect, request, redirect
 from time import timezone
-from flask import Flask, flash,render_template,url_for, request, redirect
 from forms import RegistrationForm,LoginForm
 from flask_sqlalchemy import SQLAlchemy
 import os
-from sqlalchemy.sql import func 
+from sqlalchemy.sql import func
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+
 
 ####################################### DATABASE ######################################
 
@@ -15,7 +17,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #disable tracking so that a
 
 db = SQLAlchemy(app)
 
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer(),primary_key=True)
     username = db.Column(db.String(length=50), unique=True, nullable=False)
     email = db.Column(db.String(length=100), unique=True, nullable=False)
@@ -32,48 +34,99 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 @app.route('/')
 @app.route('/home')
-@app.route('/teams')
-def teams():
-    return render_template('teams.html',title="Teams")
-
-# @app.route('/intro')
-# @app.route('/introduction')
-# def intro():
-#     return render_template('introduction.html',title="Introduction")
-
-
-# @app.route('/introdetails')
-# def introdetails():
-#     return render_template('introdetails.html',title="IntroDetails")
+def home():
+    return render_template('home.html',title="Home")
+########ONCE USER IS LOGGEN IN THEN REDIRECT THEM TO DASHBOARD#######
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html',title="Dashboard")
+####################################################################
 
 
+######################### REGISTER BEGIN ####################################
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+
+    # if form.validate_on_submit():
+    #     flash(f'Account created for {form.username.data}!', 'success')
+    #     return redirect(url_for('home'))
+
     if request.method == 'POST':
         if form.validate_on_submit():
             username = request.form['username']
             email = request.form['email']
             password = request.form['password']
-            user = Users(username = username, email = email,password = password)
-            db.session.add(user)
-            db.session.commit()
-            
+        #check if the entered username and email already exist in the db.     
             new_user = Users.query.filter_by(username=username).first()#get recently registered user's username from db to confirm registration
-            flash(f'Account created for {new_user.username}!', 'success')
-            return redirect(url_for('teams'))
+            new_email = Users.query.filter_by(email=email).first()#get recently registered user's email from db to confirm registration
+   
+        #if entered username and email already exist show error
+            if new_user:
+                flash('The username is already taken.', 'danger')
+            elif new_email:
+                flash('The Email is already registered.', 'danger')
+        #else if unique usename and email then register user
+            else:
+                user = Users(username = username, email = email,password = password)             
+                db.session.add(user)
+                db.session.commit()
+                reg_user = Users.query.filter_by(username=username).first()#get recently registered user's username from db to confirm registration
+                
+                flash(f'Account created for {reg_user.username}!', 'success')
+                return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
+################################# REGISTER END ####################################
+
+################################# LOGIN BEGIN #####################################
+
+#falsk login manager
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            email = request.form['email']
+            password = request.form['password']
+            user_db = Users.query.filter_by(email=email).first()#get recently registered user's username from db to confirm registration
+            email_db = user_db.email
+            password_db = user_db.password
+            if user_db:
+                if password_db == password:
+                    login_user(user_db)
+                    flash("You have been logged in! Your username is "+str(user_db.username), 'success')
+                    return redirect(url_for('dashboard'))
+                else:
+                    flash('Login Unsuccessful. Please check email and password', 'danger')
+
     return render_template('login.html', title='Login', form=form)
+
+#create logout
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out!")
+    return redirect(url_for('home')) 
+
+################################## LOGIN END ########################################
+
+
+#prediction form
+@app.route("/predict")
+def predict():
+    return render_template('predict.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
